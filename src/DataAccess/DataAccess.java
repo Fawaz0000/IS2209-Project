1cfa5d0da538eb6ebb1988135e7b3909098861c4
Fawaz0000 <121482992@umail.ucc.ie>
2023-03-21 15:00:26 +0000

    added git ignore

---
+++ src/DataAccess/DataAccess.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DataAccess;
+
+/**
+ *
+ * @author mdalla
+ */
+import logic.User;
+import java.util.ArrayList;
+
+public class DataAccess {
+    //String variable Referencing the textfile for I/O
+    String dataPersistenceLayer;
+    ArrayList<User> storedUsers;
+
+    //CONSTRUCTOR MEHTOD FOR DATAACCESS CLASS
+    public DataAccess(){
+        this.dataPersistenceLayer = "output.txt";
+        this.storedUsers = new ArrayList<User>();
+        this.loadUsersFromFile(this.dataPersistenceLayer);}
+   
+
+    //Create User method
+    public void create(User input){
+        this.storedUsers.add(input);
+        //helper method to save changes from memory to file
+        //this.saveUsersToFile(this.dataPersistenceLayer);
+
+    }
+
+    public void write(){
+        this.saveUsersToFile(this.dataPersistenceLayer);
+    }
+
+    // Login User Method
+    public boolean dologin(User input) {
+        this.loadUsersFromFile(this.dataPersistenceLayer);
+        boolean userP = false;
+        for (int i = 0; i < this.storedUsers.size(); i++) {
+            if (this.storedUsers.get(i).getEmail().equals(input.getEmail())) {
+                if (this.storedUsers.get(i).getPassword().equals(input.getPassword())) {
+                    userP = true;
+                    break;
+                }
+            }
+        }
+        return userP;
+    }
+
+    private void loadUsersFromFile(String filename) {
+
+        //create system nuetral reference to the I/O file
+        java.io.File inFile = new java.io.File(filename);
+        //domain object from bus logic layer
+        User user= null;
+
+        //try block to catch i/o exceptions
+        try {
+            //create new scanner
+            java.util.Scanner input = new java.util.Scanner(inFile);
+
+            while (input.hasNext()) {
+                user= new User();
+                String[] lineOfCsv = input.nextLine().split(","); //split contents of line
+                //first element/ column = ID
+                user.setName(lineOfCsv[0]);
+                user.setEmail(lineOfCsv[1]);
+                user.setPassword(lineOfCsv[2]);
+
+                this.create(user);
+            }
+            input.close();
+            //basic handlign exception
+        } catch (java.io.IOException ex){
+            System.out.println("Error reading file " + ex.toString());
+        }
+    }
+
+
+
+
+    private void saveUsersToFile(String filename){
+        java.io.File out = new java.io.File(filename);
+
+        try{
+            //create file to output text to using Printwriter
+            java.io.PrintWriter output = new java.io.PrintWriter(out);
+            for (int i=0; i < this.storedUsers.size(); i++){
+                output.println(this.storedUsers.get(i).convertToCsv());
+            }
+            output.close();
+        } catch (java.io.IOException ex){
+            System.out.println("error writing to the file " + ex.toString());
+        }
+    }
+
+}
---
+++ src/loginregistration/FXMLDocumentController.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package loginregistration;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import DataAccess.DataAccess;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import logic.User;
+
+public class FXMLDocumentController {
+
+    DataAccess dataAccess = new DataAccess();
+
+    @FXML
+    private PasswordField pwlogPassword;
+
+    @FXML
+    private TextArea talogMessage;
+
+    @FXML
+    private TextArea taregMessage;
+
+    @FXML
+    private TextField tflogEmailAddress;
+
+    @FXML
+    private TextField tfregEmailAddress;
+
+    @FXML
+    private TextField tfregName;
+
+    @FXML
+    private PasswordField pwregPassword;
+
+    @FXML
+    void btnlogClear(ActionEvent event) {
+        tflogEmailAddress.setText("");
+        pwlogPassword.setText("");
+        talogMessage.setText("");
+    }
+
+    @FXML
+    void btnlogLogin(ActionEvent event) throws Exception {
+        User userToCheck = new User();
+        userToCheck.setEmail(tflogEmailAddress.getText());
+        userToCheck.setPassword(pwlogPassword.getText());
+
+        boolean userP = dataAccess.dologin(userToCheck);
+        
+        if (userP == true) {
+            talogMessage.setText("User Found");
+            Stage stage = (Stage) (Stage)((Node) event.getSource()).getScene().getWindow();
+            Parent root= FXMLLoader.load(getClass().getResource("Menu.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Menu");
+            stage.show();
+        }
+        else {
+            talogMessage.setText("User Not Found");
+        }
+    }
+
+    @FXML
+    void btnregClear(ActionEvent event) {
+        tfregName.setText("");
+        tfregEmailAddress.setText("");
+        pwregPassword.setText("");
+        taregMessage.setText("");
+       
+
+    }
+
+    @FXML
+    void btnregRegister(ActionEvent event) {
+        User userToCreate = new User();
+        userToCreate.setName(tfregName.getText());
+        userToCreate.setEmail(tfregEmailAddress.getText());
+        userToCreate.setPassword(pwregPassword.getText());
+
+        dataAccess.create(userToCreate);
+        dataAccess.write();
+        taregMessage.setText("User Registered");
+
+    }
+
+}
